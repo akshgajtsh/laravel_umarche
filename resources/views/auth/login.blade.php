60c2fae12cfc37253b573b5adca98ae4b159e13c
Takayoshi-kobayashi <taka4677@ezweb.ne.jp>
2025-04-26 18:58:39 +0900

    add: validation Language

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
         <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
@@ -17,8 +17,7 @@
 
             <x-text-input id="password" class="block mt-1 w-full"
                 type="password"
-                name="password"
-                required autocomplete="current-password" />
+                name="password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
