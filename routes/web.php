c2375b5c74d7c5fd11d585886a953e0271041bfd
Takayoshi-kobayashi <taka4677@ezweb.ne.jp>
2025-05-07 15:19:05 +0900

    add:ServiceContainer

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ComponentTestController;
+use App\Http\Controllers\LifeCycleTestController;
 
 
 /*
@@ -32,6 +33,7 @@ Route::middleware('auth')->group(function () {
 
 Route::get('/component-test1', [ComponentTestController::class, 'showComponent1']);
 Route::get('/component-test2', [ComponentTestController::class, 'showComponent2']);
+Route::get('/servicecontainer', [LifeCycleTestController::class, 'showServiceContainer']);
 
 
 require __DIR__ . '/auth.php';
