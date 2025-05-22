a6d28dfc349d1c7db568e6057c693673e7137038
Takayoshi-kobayashi <taka4677@ezweb.ne.jp>
2025-05-22 20:28:27 +0900

    add: redairect

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -7,11 +7,21 @@ use Illuminate\Http\Request;
 
 class Authenticate extends Middleware
 {
+    protected $user_route = 'user.login';
+    protected $owner_route = 'owner.login';
+    protected $admin_route = 'admin.login';
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        // return $request->expectsJson() ? null : route('login');
+        if($request->routeIs('owner.*')){
+            return route($this->owner_route);
+        } elseif($request->routeIs('admin.*')){
+            return route($this->admin_route);
+        } else {
+            return route($this->user_route);
+        }
     }
 }
