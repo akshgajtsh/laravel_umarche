7fc8c9d7c61cbaf337c57c3548dc305d6602bd17
Takayoshi-kobayashi <taka4677@ezweb.ne.jp>
2025-05-24 10:31:48 +0900

    add: multiLogin

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -20,13 +20,6 @@ class RedirectIfAuthenticated
      */
     public function handle(Request $request, Closure $next, string ...$guards): Response
     {
-        // $guards = empty($guards) ? [null] : $guards;
-
-        // foreach ($guards as $guard) {
-        //     if (Auth::guard($guard)->check()) {
-        //         return redirect(RouteServiceProvider::HOME);
-        //     }
-        // }
         if (Auth::guard(self::GUARD_USER)->check() && $request->routeIs('user.*')) {
             return redirect(RouteServiceProvider::HOME);
         }
