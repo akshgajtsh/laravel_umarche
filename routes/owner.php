13fa43ad42f0b71174415236d1b48a5c2c628b95
Takayoshi-kobayashi <taka4677@ezweb.ne.jp>
2025-06-28 14:36:36 +0900

    add: Imagetable

--- routes/owner.php
+++ routes/owner.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\Owner\Auth\PasswordController;
 use App\Http\Controllers\Owner\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Owner\Auth\RegisteredUserController;
 use App\Http\Controllers\Owner\Auth\VerifyEmailController;
+use App\Http\Controllers\Owner\ImageContoroller;
 use App\Http\Controllers\Owner\ShopController;
 use Illuminate\Support\Facades\Route;
 
@@ -33,6 +34,9 @@ Route::prefix('shops')->middleware('auth:owners')->group(function () {
     Route::get('edit/{shop}', [ShopController::class, 'edit'])->name('shops.edit');
     Route::post('update/{shop}', [ShopController::class, 'update'])->name('shops.update');
 });
+
+Route::resource('images', ImageContoroller::class)
+    ->middleware('auth:owners')->except(['show']);
 
 Route::get('/dashboard', function () {
     return view('owner.dashboard');
